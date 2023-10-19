f1f8b067c00efa34275a991db1f1ed7f6a3f2ba4
j2harmony <148473182+j2harmony@users.noreply.github.com>
2023-10-19 12:51:55 -0500

    Practice

---
+++ printPractice.py
@@ -0,0 +1,12 @@
+#practice printing strings below
+
+#print Hi my name is your name (enter your name)
+
+#print What city you are from
+
+#print What is your favorite food
+
+#print your city
+
+#print the name John Smith
+
---
+++ variablePractice.py
@@ -0,0 +1,25 @@
+#create variables than print variables
+
+#enter your name
+name = 'Richard'
+
+#enter your profession
+career = 'Technology'
+
+#enter your state
+state = 'Arizona'
+
+#enter your salary
+pay = 85000
+
+#enter your desire salary
+salary = 200000
+
+
+
+name = input('What is your name:')
+
+print(name)
+print('It is nice to meet you ', name)
+
+
